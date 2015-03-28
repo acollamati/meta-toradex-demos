c06f362ce5b4c15d65bd81e19bff776ea5181ae4
Max Krummenacher <max.krummenacher@toradex.com>
2015-03-28 14:22:53 +0100

    trdx-image-fstype: pull out common files
    Pull out common files of the module specific trees into the common library
    directory and symlink the files.

--- recipes/images/files/colibri-vf/colibri-vf_bin/mk-u-boot-scripts.sh
+++ recipes/images/files/colibri-vf/colibri-vf_bin/mk-u-boot-scripts.sh
@@ -1,7 +1,1 @@
-#!/bin/sh
-for f in *.scr
-do
-	mkimage -T script -C none -n 'Flash Colibri VF' -d $f `basename $f .scr`.img
-done	
-#mkimage -T script -C none -n 'Flash Colibri VF with data from SD card' -d flash_mmc.scr flash_mmc.img
-#mkimage -T script -C none -n 'Flash Colibri VF with u-boot from SD card' -d flash_mmc_u-boot.scr flash_mmc_u-boot.img
+../../library/mk-u-boot-scripts.sh
