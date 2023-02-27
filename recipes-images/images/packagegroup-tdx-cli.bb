acollamati/meta-toradex-demos

commit ef7368d235a2c08ec72b040fdadaa994b62d3f38

packagegroups: prepare for verdin am62 addition
Related-to: ELB-4978
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-images/images/packagegroup-tdx-cli.bb
@@ -152,6 +152,9 @@ RRECOMMENDS:packagegroup-utils-tdx-cli = "\
     tdx-oak-sensors \
     util-linux \
 "
+RRECOMMENDS:packagegroup-utils-tdx-cli:am62xx = " \
+    cpuburn-a53 \
+"
 RRECOMMENDS:packagegroup-utils-tdx-cli:append:mx8-generic-bsp = " \
     cpuburn-a53 \
 "

recipes-images/images/packagegroup-tdx-graphical.bb
@@ -22,6 +22,9 @@ RRECOMMENDS:packagegroup-tdx-graphical = " \
                           packagegroup-x11-utils-tdx-graphical', \
                          '', d), d)} \
 "
+RRECOMMENDS:packagegroup-tdx-graphical:append:am62xx = " \
+    packagegroup-drm-utils-tdx-graphical \
+"
 RRECOMMENDS:packagegroup-tdx-graphical:append:mx8-nxp-bsp = " \
     packagegroup-drm-utils-tdx-graphical \
 "

commit 3ed48330988647960161aec76eb5c789cdff1d41

packagegroup-tdx-cli: am62xx: fix missing append
Due to that a bunch of utilities are not part of the image.

Fixes: ef7368d ("packagegroups: prepare for verdin am62 addition")
Related-to: ELB-4978
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-images/images/packagegroup-tdx-cli.bb
@@ -151,7 +151,7 @@ RRECOMMENDS:packagegroup-utils-tdx-cli = "\
     tdx-oak-sensors \
     util-linux \
 "
-RRECOMMENDS:packagegroup-utils-tdx-cli:am62xx = " \
+RRECOMMENDS:packagegroup-utils-tdx-cli:append:am62xx = " \
     cpuburn-a53 \
 "
 RRECOMMENDS:packagegroup-utils-tdx-cli:append:mx8-generic-bsp = " \